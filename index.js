1c0f47dbc016fa1df541b5892c576f5e9552e016
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2023-06-24 15:57:43 +0530

    final

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ let backButton2 = document.querySelector(".backBtn2");
 let addTaskPopUpSecondPage = document.querySelector(".addTaskPopUpSecondPage");
 let addTaskButton = document.querySelector(".addTaskButton");
 let closeTaskButton = document.querySelector(".closeTaskButton");
-let head1 = document.getElementsByClassName("head1");
+let headingFirstPage = document.getElementsByClassName("headingFirstPage");
 let popup = document.getElementsByClassName("addTask");
 let popup2 = document.getElementsByClassName("addItem");
-let addItemButton = document.querySelector(".addItemButton");
-let head2 = document.getElementsByClassName("head2");
-let head2Text = document.getElementsByClassName("head2Text");
-let todo = document.getElementsByClassName("todos");
-let todo2 = document.getElementsByClassName("todo2");
-let closeItemButton = document.querySelector(".closeItemButton");
+let addSubtaskConButton = document.querySelector(".addSubtaskConButton");
+let headingSecondPage = document.getElementsByClassName("headingSecondPage");
+let headingSecondPageText = document.getElementsByClassName(
+  "headingSecondPageText"
+);
+let toDoCardContainer = document.getElementsByClassName("toDoCardContainers");
+let toDoCardContainer2 = document.getElementsByClassName("toDoCardContainer2");
+let closeSubtaskConButton = document.querySelector(".closeSubtaskConButton");
 
 // codeing start with addEventListener
 addTaskPopUp.addEventListener("click", () => {
-  addPopup();
+  addNewPopUp();
 });
 addTaskPopUp2.addEventListener("click", () => {
-  addPopup();
+  addNewPopUp();
 });
 backButton.addEventListener("click", () => {
   back();
@@ -30,7 +32,7 @@ backButton2.addEventListener("click", () => {
   back();
 });
 addTaskPopUpSecondPage.addEventListener("click", () => {
-  addPopup();
+  addNewPopUp();
 });
 addTaskButton.addEventListener("click", () => {
   addTask();
@@ -38,29 +40,29 @@ addTaskButton.addEventListener("click", () => {
 closeTaskButton.addEventListener("click", () => {
   closeTask();
 });
-addItemButton.addEventListener("click", () => {
+addSubtaskConButton.addEventListener("click", () => {
   addItem();
 });
-closeItemButton.addEventListener("click", () => {
+closeSubtaskConButton.addEventListener("click", () => {
   closeItem();
 });
 
 // function start
-function addPopup() {
-  head1[0].classList.add("blur1");
-  head2[0].classList.add("blur1");
+function addNewPopUp() {
+  headingFirstPage[0].classList.add("blur1");
+  headingSecondPage[0].classList.add("blur1");
 
   popup[0].style.display = "block";
 }
 
 function closeTask() {
   popup[0].style.display = "none";
-  head1[0].classList.remove("blur1");
-  head2[0].classList.remove("blur1");
+  headingFirstPage[0].classList.remove("blur1");
+  headingSecondPage[0].classList.remove("blur1");
 }
 var idCount = 0;
 var count = 0;
-let todoCardId = 0;
+let toDoCardContainerCardId = 0;
 let child;
 let headChild;
 let childMainDiv;
@@ -87,45 +89,45 @@ function addTask() {
 }
 
 function createCard(id, cardTittle, count) {
-  let todoDiv = document.createElement("div");
-  todoDiv.setAttribute("id", `${id}`);
+  let toDoCardContainerDiv = document.createElement("div");
+  toDoCardContainerDiv.setAttribute("id", `${id}`);
   let heading = document.createElement("p");
   let button1 = document.createElement("img");
   let button2 = document.createElement("img");
 
   let mainDiv = document.createElement("div");
   mainDiv.setAttribute("id", `mainDiv${id}`);
-  todo[0].appendChild(todoDiv);
-  todoDiv.appendChild(heading);
-  todoDiv.append(button1);
-  todoDiv.append(button2);
-  todoDiv.appendChild(mainDiv);
+  toDoCardContainer[0].appendChild(toDoCardContainerDiv);
+  toDoCardContainerDiv.appendChild(heading);
+  toDoCardContainerDiv.append(button1);
+  toDoCardContainerDiv.append(button2);
+  toDoCardContainerDiv.appendChild(mainDiv);
 
   heading.innerHTML = `${cardTittle}`;
 
   button2.src = "./edit.png";
   button1.src = "./trash.png";
   mainDiv.classList.add("mainDiv");
-  todoDiv.classList.add("todoscard");
+  toDoCardContainerDiv.classList.add("toDoCardContainerscard");
   button1.classList.add("addCardBtn1");
   button2.classList.add("addCardBtn2");
   heading.classList.add("line");
 
   heading.addEventListener("click", () => {
-    head1[0].style.display = "none";
-    head2[0].style.display = "block";
+    headingFirstPage[0].style.display = "none";
+    headingSecondPage[0].style.display = "block";
     mainDiv.classList.add("mainDivBack");
-    head2Text[0].innerHTML = `${cardTittle}`;
-    todoCardId = todoDiv.getAttribute("id");
-    headChild = document.getElementById(`${todoCardId}`);
+    headingSecondPageText[0].innerHTML = `${cardTittle}`;
+    toDoCardContainerCardId = toDoCardContainerDiv.getAttribute("id");
+    headChild = document.getElementById(`${toDoCardContainerCardId}`);
     console.log(headChild);
-    todo2[0].appendChild(headChild);
+    toDoCardContainer2[0].appendChild(headChild);
     // here headChaild is a taskParentDiv
   });
 
   button1.addEventListener("click", () => {
     count--;
-    todoDiv.remove();
+    toDoCardContainerDiv.remove();
     if (count === 0) {
       let checkItems = document.querySelector(".checkItem");
       checkItems.style.display = "block";
@@ -133,76 +135,76 @@ function createCard(id, cardTittle, count) {
   });
 
   button2.addEventListener("click", () => {
-    head1[0].classList.add("blur1");
-    head2[0].classList.add("blur1");
+    headingFirstPage[0].classList.add("blur1");
+    headingSecondPage[0].classList.add("blur1");
     popup2[0].style.display = "block";
 
     // this line is use to finding the main parent of the box
     // jisme sub kuch rakha hai
-    todoCardId = todoDiv.getAttribute("id");
+    toDoCardContainerCardId = toDoCardContainerDiv.getAttribute("id");
     // this line is use to targeting the subtaskContainner
-    child = document.getElementById(`${todoCardId}`).children;
+    child = document.getElementById(`${toDoCardContainerCardId}`).children;
     childMainDiv = child[3];
     // here childMainDiv is subtaskContainner
   });
 }
 
 function closeItem() {
-  head1[0].classList.remove("blur1");
-  head2[0].classList.remove("blur1");
+  headingFirstPage[0].classList.remove("blur1");
+  headingSecondPage[0].classList.remove("blur1");
   popup2[0].style.display = "none";
 }
 
 function addItem() {
   popup2[0].style.display = "none";
-  head1[0].classList.remove("blur1");
-  head2[0].classList.remove("blur1");
+  headingFirstPage[0].classList.remove("blur1");
+  headingSecondPage[0].classList.remove("blur1");
 
   let itemDiv = document.createElement("div");
   let itemText = document.createElement("div");
-  let itemButton = document.createElement("button");
+  let SubtaskConButton = document.createElement("button");
 
   console.log(childMainDiv);
   childMainDiv.appendChild(itemDiv);
   itemDiv.appendChild(itemText);
-  itemDiv.appendChild(itemButton);
+  itemDiv.appendChild(SubtaskConButton);
 
   itemDiv.classList.add("itemDiv");
   itemText.classList.add("itemText");
-  itemButton.classList.add("itemButton");
+  SubtaskConButton.classList.add("SubtaskConButton");
 
   let itemInput = document.querySelector(".cardItem").value;
   // subtask Heading
   if (itemInput) {
     itemText.innerHTML = `${itemInput}`;
-    itemButton.innerHTML = " Done";
+    SubtaskConButton.innerHTML = " Done";
   } else {
     itemText.innerHTML = `unKnow`;
-    itemButton.innerHTML = " Done";
+    SubtaskConButton.innerHTML = " Done";
   }
 
   itemText.innerHTML = `${itemInput}`;
-  itemButton.innerHTML = " Done";
+  SubtaskConButton.innerHTML = " Done";
 
-  itemButton.addEventListener("click", () => {
+  SubtaskConButton.addEventListener("click", () => {
     itemText.style.textDecoration = "line-through";
     itemText.style.color = "red";
-    itemButton.style.display = "none";
+    SubtaskConButton.style.display = "none";
     itemText.style.marginLeft = "55px";
 
     itemText.addEventListener("click", () => {
       itemText.style.textDecoration = "none";
       itemText.style.color = "black";
-      itemButton.style.display = "block";
+      SubtaskConButton.style.display = "block";
       itemText.style.marginLeft = "0px";
     });
   });
 }
 
 function back() {
-  head1[0].style.display = "block";
-  head2[0].style.display = "none";
+  headingFirstPage[0].style.display = "block";
+  headingSecondPage[0].style.display = "none";
   // here headChaild is a taskParentDiv
-  todo[0].appendChild(headChild);
+  toDoCardContainer[0].appendChild(headChild);
   mainDiv.classList.remove("mainDivBack");
 }
